5332a0d39982a6b7246a4cd0efb5b95436f58f09
ahmed-elghazi <126707449+ahmed-elghazi@users.noreply.github.com>
2023-11-02 18:44:09 -0500

    Update Seat.java

--- Seat.java
+++ Seat.java
@@ -1,36 +1,47 @@
+// This class represents a seat within an auditorium.
 public class Seat {
-    private char colChar;
-    private char ticketType;
-    private int row;
 
+    // Attributes of the Seat class
+    private char colChar;    // Represents the column character (e.g., 'A', 'B', etc.).
+    private char ticketType; // Represents the type of ticket for the seat (e.g., 'A' for adult).
+    private int row;         // Represents the row number of the seat.
 
+    // Default constructor: Initializes the seat with default values.
     public Seat(){
-        ticketType = 'X';
-        colChar = '!';
-        row = -1;
+        ticketType = 'X';  // Represents an uninitialized or invalid ticket type.
+        colChar = '!';     // Represents an uninitialized or invalid column character.
+        row = -1;          // Represents an uninitialized or invalid row number.
+    }
 
-    }
+    // Parameterized constructor: Initializes the seat with the provided data.
     public Seat(char seat, char ticket, int r){
-        ticketType = ticket;
-        colChar = seat;
-        row = r;
+        ticketType = ticket;  // Sets the ticket type for the seat.
+        colChar = seat;       // Sets the column character for the seat.
+        row = r;              // Sets the row number for the seat.
     }
+
+    // Method to set the ticket type for the seat.
     public void setSeat(char t) {
         ticketType = t;
     }
 
+    // Returns the ticket type for the seat.
     public char getSeat(){
         return ticketType;
     }
+
+    // Returns the column character of the seat.
     public char getColChar(){
         return colChar;
     }
 
+    // Returns the row number of the seat.
     public int getRow(){
         return row;
     }
 
+    // Returns a string representation of the ticket type for the seat.
     public String toString() {
-        return "" + row + colChar;
+        return String.valueOf(ticketType);
     }
 }
